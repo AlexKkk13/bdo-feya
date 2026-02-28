e6cc75f67e84ede6992fe19ab14457475f564699
AlexKkk13 <AlexKov180614@gmail.com>
2026-02-28 03:05:36 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+// Функция для открытия картинки
+function openImage(src) {
+    const modal = document.getElementById("imageModal");
+    const fullImg = document.getElementById("fullImage");
+    
+    modal.style.display = "flex";
+    fullImg.src = src;
+}
+
+// Функция для закрытия
+function closeImage() {
+    document.getElementById("imageModal").style.display = "none";
+}
+
+// Автоматически добавляем возможность клика всем картинкам в контенте
+document.querySelectorAll('.image-wrapper img').forEach(img => {
+    img.style.cursor = 'zoom-in';
+    img.onclick = function() {
+        openImage(this.src);
+    }
+});
